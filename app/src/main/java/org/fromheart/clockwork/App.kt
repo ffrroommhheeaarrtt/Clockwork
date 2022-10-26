b42f475be2efb5924dcb910d3acb8264ee7b9c75
Fromheart <ffrroommhheeaarrtt@gmail.com>
2022-10-26 18:29:40 +0300

    fixed running timer bug

--- app/src/main/java/org/fromheart/clockwork/App.kt
+++ app/src/main/java/org/fromheart/clockwork/App.kt
@@ -2,10 +2,12 @@ package org.fromheart.clockwork
 
 import android.app.Application
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import org.fromheart.clockwork.data.AppDatabase
+import org.fromheart.clockwork.data.model.TimerStatus
 import org.fromheart.clockwork.repository.AlarmRepository
+import org.fromheart.clockwork.repository.TimerRepository
 
 class App : Application() {
 
@@ -13,10 +15,16 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        CoroutineScope(Job()).launch {
+        CoroutineScope(SupervisorJob()).launch {
             val alarmRepository = AlarmRepository(database.alarmDao())
+            val timerRepository = TimerRepository(database.timerDao())
+
             alarmRepository.alarmDao.getOpenAlarm()?.let { alarm ->
                 alarmRepository.alarmDao.update(alarm.copy(isOpened = false))
+            }
+
+            timerRepository.timerDao.getRunningTimer()?.let { timer ->
+                timerRepository.timerDao.update(timer.copy(status = TimerStatus.PAUSE.number))
             }
         }
     }
